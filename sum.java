65112e966443819fba46693a48398c66f4702070
Sajag_Mishra1370 <ffboy1224@gmail.com>
2025-08-12 09:41:53 +0530

    Here is the Java Basic.
    Oops Starting.

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = scn.nextInt();
+        System.out.print("Enter a number: ");
+        int b = scn.nextInt();
+        System.out.println(b+a);
+    }
+}
---
+++ userInput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class userInput {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = scn.nextInt();
+        
+        System.out.print(a);
+    }
+}
